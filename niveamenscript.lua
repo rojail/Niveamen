278c448629472bc1512a3157e4e10192431ac299
rojail <128169362+rojail@users.noreply.github.com>
2023-03-17 19:05:35 +0300

    Update niveamenscript.lua

--- niveamenscript.lua
+++ niveamenscript.lua
@@ -1,5 +1,3 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
-
 local Window = Library.CreateLib("NIVEAMEN", "RJTheme3")
 
 local Tab = Window:NewTab("Character")
